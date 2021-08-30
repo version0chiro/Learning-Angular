f18b899060f60e257f648159048fc9f0b0435422
Chiro <sachinsinghroxx@gmail.com>
2021-08-30 12:30:05 +0530

    made async

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,8 +13,15 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  heroes = HEROES;
-  constructor() {}
+  heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
 
-  ngOnInit(): void {}
+    // this.heroes = this.heroService.getHeroes();
+  }
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
