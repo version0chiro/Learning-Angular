8c884230ef8df1ca4d38265085114d14343b07ae
Chiro <sachinsinghroxx@gmail.com>
2021-08-31 13:02:40 +0530

    Completed navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,24 +9,13 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(
-      `HeroesComponent: Selected hero id ${this.selectedHero.id}`
-    );
-  }
   heroes: Hero[] = [];
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
 
     // this.heroes = this.heroService.getHeroes();
   }
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
