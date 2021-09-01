007594fd28a72ab63bd2fea76439e9cbd6356525
Chiro <sachinsinghroxx@gmail.com>
2021-09-01 16:13:12 +0530

    Added a search function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -72,4 +72,18 @@ export class HeroService {
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((_) =>
+        _.length
+          ? this.log(`found heroes matching "${term}"`)
+          : this.log(`no heroes matching "${term}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
